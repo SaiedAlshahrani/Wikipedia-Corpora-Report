1d95e5b3e74b969f876837cd26c09de9bb3bfa0d
Saied Alshahrani <101026149+SaiedAlshahrani@users.noreply.github.com>
2023-08-21 21:27:53 -0400

    Update update-daemon.sh

--- update-daemon.sh
+++ update-daemon.sh
@@ -1,9 +1,7 @@
-pip3 install --quiet selenium==3.141.0 geckodriver-autoinstaller==0.1.0 python-dateutil
-
 python update-metadata.py
 
 # git lfs install
-git clone https://huggingface.co/datasets/SaiedAlshahrani/test
+git clone https://huggingface.co/datasets/SaiedAlshahrani/Wikipedia-Corpora-Report
 cd Wikipedia-Corpora-Report/
 
 head -n1 ../English--Wikipedia--Metadata.csv > Wikipedia-Corpora-Report.csv
