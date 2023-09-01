3c3b13aa520db08766bd963868739b36700b83cb
Saied Alshahrani <101026149+SaiedAlshahrani@users.noreply.github.com>
2023-09-01 11:06:06 -0400

    Add Secret Token

--- report.py
+++ report.py
@@ -111,8 +111,9 @@ display_data_table = st.checkbox(f'Display metadata in a table.', value=False)
 
 @st.cache_data
 def fetch_metadata_dataset():
-        token = "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"
-        dataset = datasets.load_dataset("SaiedAlshahrani/Wikipedia-Corpora-Report", split="train", use_auth_token=token)
+        # token = "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"
+        HF_TOKEN = st.secrets["HF_TOKEN"]
+        dataset = datasets.load_dataset("SaiedAlshahrani/Wikipedia-Corpora-Report", split="train", use_auth_token=HF_TOKEN)
         dataset = dataset.to_pandas()
         return dataset
 
