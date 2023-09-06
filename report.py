5558b1e8d7c2dbe4d5197925249aa5f1f1d64128
Saied Alshahrani <101026149+SaiedAlshahrani@users.noreply.github.com>
2023-09-06 14:03:17 -0400

    Update report.py

--- report.py
+++ report.py
@@ -152,8 +152,8 @@ with col1:
     display_data_table = st.checkbox(f'Display metadata in a table.', value=False)
 
 with col2:
-    wiki_metadata = wiki_metadata.to_csv()
     wiki_metadata = wiki_metadata.reset_index(drop=True, inplace=True)
+    wiki_metadata = wiki_metadata.to_csv()
     download_button = st.download_button(label="Download Metadata", data=wiki_metadata,
                                         file_name=f'{selected_language.split("(")[0].strip(" ")}-Metadata-{retrieval_date}.csv', mime='text/csv',)
 
